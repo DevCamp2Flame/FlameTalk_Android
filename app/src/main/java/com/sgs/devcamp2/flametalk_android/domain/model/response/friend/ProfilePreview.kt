9dc2120c151e5f71009bc4f5712559b7b762a5cc
paksuua <paksuua@gmail.com>
2022-01-17 15:34:29 +0900

    Update: ProfilePreview model
    id, background 추가

--- app/src/main/java/com/sgs/devcamp2/flametalk_android/domain/model/response/friend/ProfilePreview.kt
+++ app/src/main/java/com/sgs/devcamp2/flametalk_android/domain/model/response/friend/ProfilePreview.kt
@@ -1,7 +1,18 @@
 package com.sgs.devcamp2.flametalk_android.domain.model.response.friend
 
+/**
+ *  ProfilePreview
+ *
+ *   nickname 유저 닉네임
+ *   image 프로필 이미지
+ *   description 상태 메세지
+ *   backgroundImage 배경 이미지
+ */
+
 data class ProfilePreview(
+    val userId: Int,
     val nickname: String,
     val image: String?,
-    val description: String?
+    val description: String?,
+    val backgroundImage: String?
 )
