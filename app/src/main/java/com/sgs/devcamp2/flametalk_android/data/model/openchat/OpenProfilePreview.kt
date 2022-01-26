1015764e5acf8a496664f0bfd7b57aa75adc1e97
paksuua <paksuua@gmail.com>
2022-01-26 09:47:34 +0900

    Add: Open Chat data model
    오픈채팅방에서 쓰이는 리스트 아이템의 데이터 모델

---
+++ app/src/main/java/com/sgs/devcamp2/flametalk_android/data/model/openchat/OpenProfilePreview.kt
@@ -0,0 +1,23 @@
+package com.sgs.devcamp2.flametalk_android.data.model.openchat
+
+import android.os.Parcelable
+import kotlinx.android.parcel.Parcelize
+
+/**
+ *  OpenChatPreview
+ *
+ *   title 채팅방 제목
+ *   desc 채팅방 소개글
+ *   hostProfile 방장 프로필 이미지
+ *   count 참여인원
+ *   thumbnail 채팅방 프로필 이미지
+ */
+
+@Parcelize
+data class OpenProfilePreview(
+    val title: String,
+    val desc: String,
+    val hostProfile: String,
+    val count: Int,
+    val thumbnail: String
+) : Parcelable
