18e2bb49b158e451df8e6a1987acf90e8e4e7111
paksuua <paksuua@gmail.com>
2022-01-26 14:32:18 +0900

    Add: 토큰 재발급 통신 api

---
+++ app/src/main/java/com/sgs/devcamp2/flametalk_android/network/response/sign/RenewTokenResponse.kt
@@ -0,0 +1,45 @@
+package com.sgs.devcamp2.flametalk_android.network.response.sign
+
+import androidx.annotation.Keep
+import com.google.gson.annotations.SerializedName
+
+/**
+ * @author 박소연
+ * @created 2022/01/26
+ * @updated
+ * @desc 토큰 재발급 Response Body
+ *
+ * [User Login API]
+ * status	    int 	상태 코드
+ * message	    String	status 메시지
+ *
+ * userId	    String	이메일
+ * nickname	    String	별명
+ * status	    String	권한 & 상태
+ * accessToken	String	resource server 통신에 활용
+ * refreshToken	String	토큰 재발급에 이용됨
+ */
+
+@Keep
+data class RenewTokenResponse(
+    @SerializedName("status")
+    val status: Int,
+    @SerializedName("message")
+    val message: String,
+    @SerializedName("status")
+    val data: Result
+) {
+    @Keep
+    data class Result(
+        @SerializedName("userId")
+        val userId: String,
+        @SerializedName("nickname")
+        val nickname: String,
+        @SerializedName("status")
+        val status: String,
+        @SerializedName("accessToken")
+        val accessToken: String,
+        @SerializedName("refreshToken")
+        val refreshToken: String
+    )
+}
--- app/src/main/java/com/sgs/devcamp2/flametalk_android/network/service/UserService.kt
+++ app/src/main/java/com/sgs/devcamp2/flametalk_android/network/service/UserService.kt
@@ -3,12 +3,10 @@ package com.sgs.devcamp2.flametalk_android.network.service
 import com.sgs.devcamp2.flametalk_android.network.request.sign.SigninRequest
 import com.sgs.devcamp2.flametalk_android.network.request.sign.SignupRequest
 import com.sgs.devcamp2.flametalk_android.network.response.sign.EmailCheckResponse
+import com.sgs.devcamp2.flametalk_android.network.response.sign.RenewTokenResponse
 import com.sgs.devcamp2.flametalk_android.network.response.sign.SigninResponse
 import com.sgs.devcamp2.flametalk_android.network.response.sign.SignupResponse
-import retrofit2.http.Body
-import retrofit2.http.GET
-import retrofit2.http.POST
-import retrofit2.http.Query
+import retrofit2.http.*
 
 interface UserService {
     // 회원가입
@@ -28,4 +26,8 @@ interface UserService {
     suspend fun getEmailCheck(
         @Query("email") email: String
     ): EmailCheckResponse
+
+    // 토큰 재발급
+    @GET("/api/auth/token")
+    suspend fun getRenewToken(): RenewTokenResponse
 }
