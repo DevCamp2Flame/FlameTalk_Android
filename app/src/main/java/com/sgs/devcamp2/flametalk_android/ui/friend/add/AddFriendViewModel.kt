dfc6b424c3d5336b5f98561c766414d539d49360
Soyeon Park <paksuua@naver.com>
2022-02-11 00:23:12 +0900

    Add: Add Friend
    - UI 로직 구현
    - api 통신 로직 구현 (테스트 전)

---
+++ app/src/main/java/com/sgs/devcamp2/flametalk_android/ui/friend/add/AddFriendViewModel.kt
@@ -0,0 +1,110 @@
+package com.sgs.devcamp2.flametalk_android.ui.friend.add
+
+import android.content.Context
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sgs.devcamp2.flametalk_android.data.dummy.getDummyProfiles
+import com.sgs.devcamp2.flametalk_android.data.model.ProfilePreview
+import com.sgs.devcamp2.flametalk_android.network.dao.UserDAO
+import com.sgs.devcamp2.flametalk_android.network.repository.FriendRepository
+import com.sgs.devcamp2.flametalk_android.network.repository.ProfileRepository
+import com.sgs.devcamp2.flametalk_android.network.request.friend.AddFriendRequest
+import com.sgs.devcamp2.flametalk_android.network.response.friend.AddFriendResponse
+import dagger.Lazy
+import dagger.hilt.android.lifecycle.HiltViewModel
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import timber.log.Timber
+
+@HiltViewModel
+class AddFriendViewModel @Inject constructor(
+    @ApplicationContext private val context: Context,
+    private val userDAO: UserDAO,
+    private val friendRepository: Lazy<FriendRepository>,
+    private val profileRepository: Lazy<ProfileRepository>
+) : ViewModel() {
+    private var dummyProfileData: List<ProfilePreview> = getDummyProfiles()
+
+    // 유저 프로필 리스트
+    private val _profiles = MutableStateFlow<List<ProfilePreview>>(emptyList())
+    val profiles: MutableStateFlow<List<ProfilePreview>> = _profiles
+
+    // 선택된 프로필
+    private val _profileId = MutableStateFlow(0L)
+    val profileId = _profileId.asStateFlow()
+
+    // 닉네임
+    private val _nickname = MutableStateFlow("")
+    val nickname = _nickname.asStateFlow()
+
+    // 추가된 친구 데이터
+    private val _friendData: MutableStateFlow<AddFriendResponse.Data?> = MutableStateFlow(null)
+    val friendData = _friendData.asStateFlow()
+
+    // 친구 추가 성공 여부
+    private val _isSuccess: MutableStateFlow<Boolean?> = MutableStateFlow(null)
+    val isSuccess = _isSuccess.asStateFlow()
+
+    private val _message = MutableStateFlow<String?>(null)
+    val message = _message.asStateFlow()
+
+    init {
+        // 유저 프로필 리스트 불러오기
+        // getProfileList()
+
+        _profiles.value = dummyProfileData
+        _nickname.value = "소연"
+//        viewModelScope.launch {
+//            userDAO.user.collect {
+//                _nickname.value = it!!.nickname
+//            }
+//        }
+    }
+
+    fun clickedProfile(profileId: Long) {
+        _profileId.value = profileId
+    }
+
+    fun addFriend(phoneNumber: String) {
+        viewModelScope.launch {
+            try {
+                val request = AddFriendRequest(_profileId.value, phoneNumber)
+                val response = friendRepository.get().postFriendAdd(request)
+
+                if (response.status == 201) {
+                    _friendData.value = response.data
+                    _isSuccess.value = true
+                } else {
+                    _isSuccess.value = true
+                    _message.value = response.message
+                }
+                _message.value = null
+                _isSuccess.value = null
+            } catch (ignored: Throwable) {
+                Timber.d("Fail Response: $ignored")
+            }
+        }
+    }
+
+    private fun getProfileList() {
+        viewModelScope.launch {
+            try {
+                val response = profileRepository.get().getProfileList()
+
+                if (response.status == 200) {
+                    _profiles.value = response.data.profiles
+
+                    // Result
+                    Timber.d("User Profile ${_profiles?.value}")
+                } else {
+                    _message.value = response.message
+                }
+            } catch (ignored: Throwable) {
+                Timber.d("Fail Response: $ignored")
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/sgs/devcamp2/flametalk_android/ui/friend/add/SelectProfileAdapter.kt
@@ -0,0 +1,75 @@
+package com.sgs.devcamp2.flametalk_android.ui.friend.add
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.sgs.devcamp2.flametalk_android.data.model.ProfilePreview
+import com.sgs.devcamp2.flametalk_android.databinding.ItemVerticalProfileBinding
+import com.sgs.devcamp2.flametalk_android.util.SimpleDiffUtilCallback
+
+/**
+ * @author 박소연
+ * @created 2022/02/11
+ * @updated 2022/02/11
+ * @desc 친구 추가 시 보여줄 프로필을 선택해야 한다.
+ *       초기에는 선택이 안되어있음. 한번 선택을 하면 해제할 수 없음. single 선택임
+ */
+// TODO: 초기에는 선택이 안되어있음
+// TODO: 한번 선택을 하면 해제할 수 없음. single 선택임
+
+class SelectProfileAdapter(
+    private val context: Context,
+    private val nickname: String,
+    private val onClicked: (ProfilePreview) -> Unit
+) : ListAdapter<ProfilePreview, SelectProfileAdapter.ViewHolder>(SimpleDiffUtilCallback()) {
+    var data = listOf<ProfilePreview>()
+    var clickedItem = 0L
+    var clickedView: View? = null
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        return ViewHolder(
+            ItemVerticalProfileBinding.inflate(
+                LayoutInflater.from(context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return data.size
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(data[position])
+    }
+
+    inner class ViewHolder(
+        private var binding: ItemVerticalProfileBinding
+    ) : RecyclerView.ViewHolder(binding.root) {
+
+        fun bind(data: ProfilePreview) {
+            Glide.with(itemView).load(data.imageUrl).into(binding.imgVerticalProfile)
+            binding.tvVerticalProfileNickname.text = nickname
+
+            if (data.id != clickedItem) { // 클릭되지 않았고 선택되지 않은 아이템
+                binding.vVerticalProfile.isSelected = false
+            }
+
+            binding.vVerticalProfile.setOnClickListener {
+                onClicked.invoke(data)
+                clickedItem = data.id
+                clickedView?.isSelected = false
+
+                if (clickedView != it) { // 재클릭
+                    clickedView = it
+                    it.isSelected = true
+                }
+            }
+        }
+    }
+}
